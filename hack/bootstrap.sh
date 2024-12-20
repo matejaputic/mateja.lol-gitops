7001047e382ddf9ce97da87b956c59ad67f8d5fb
Mateja <mateja.putic@gmail.com>
2024-12-20 12:39:48 -0500

    I guess we're goin cilium

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -3,20 +3,21 @@
 # Thanks to [davralin/cluster-ops](https://github.com/davralin/cluster-ops)
 # Thanks to [xunholy/k8s-ops](https://github.com/xunholy/k8s-gitops)
 
-# helm upgrade \
-#     cilium \
-#     cilium/cilium \
-#     --install \
-#     --version 1.16.4 \
-#     --namespace kube-system \
-#     --set ipam.mode=kubernetes \
-#     --set kubeProxyReplacement=true \
-#     --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
-#     --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
-#     --set cgroup.autoMount.enabled=false \
-#     --set cgroup.hostRoot=/sys/fs/cgroup \
-#     --set k8sServiceHost=localhost \
-#     --set k8sServicePort=7445
+helm upgrade \
+    cilium \
+    cilium/cilium \
+    --install \
+    --version 1.16.4 \
+    --namespace kube-system \
+    --set ipam.mode=kubernetes \
+    --set kubeProxyReplacement=true \
+    --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
+    --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
+    --set cgroup.autoMount.enabled=false \
+    --set cgroup.hostRoot=/sys/fs/cgroup \
+    --set k8sServiceHost=localhost \
+    --set k8sServicePort=7445 \
+    --wait --timeout=5m
 
 # HELM INSTALL FLUX
 helm upgrade \
@@ -28,7 +29,8 @@ helm upgrade \
     --namespace flux-system \
     --set imageAutomationController.create=false \
     --set imageReflectionController.create=false \
-    --set clusterDomain=cluster.internal
+    --set clusterDomain=cluster.internal \
+    --wait --timeout=5m
 
 # DECRYPT AND APPLY THE DATA ENCRYPTION KEY (DEK)
 # ASSUMES YOU HAVE THE KEY ENCRYPTION KEY (KEK) LOCALLY
@@ -51,4 +53,5 @@ helm upgrade \
     --set gitRepository.spec.url=https://github.com/matejaputic/mateja.lol-gitops \
     --set gitRepository.spec.ref.branch=main \
     --set kustomization.spec.path=./kubernetes/flux-system \
-    --set kustomization.spec.prune=true
+    --set kustomization.spec.prune=true \
+    --wait --timeout=5m
